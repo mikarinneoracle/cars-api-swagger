04a838c8174ee1d21562608b95011beacf0a4a46
Mika Rinne <mika.rinne@oracle.com>
2025-10-17 17:40:11 +0300

    apigw deployment fixes

--- apigw/update-script.sh
+++ apigw/update-script.sh
@@ -1,7 +1,9 @@
-ci_private_ip="10.0.1.177"
-deployment_ocid="ocid1.apideployment.oc1.eu-frankfurt-1.amaaaaaauevftmqaexvtoknbc5wv4mqcsmmiwmpcupweqrhzisgaxy76ky6q"
+source settings.env
+read -p "Enter Container Instances private IP:" ci_private_ip
 timestamp=$(date +%s)
-sed "s|{CI_PRIVATE_IP}|$ci_private_ip|g" deployment.json > "deployment_$timestamp.json"
+sed "s|{CI_PRIVATE_IP}|$ci_private_ip|g" deployment.json > "_deployment_$timestamp.json"
+sed "s|{AUTH_FUNC_OCID}|$auth_function_ocid|g" "_deployment_$timestamp.json" > "deployment_$timestamp.json"
 cat "deployment_$timestamp.json"
 oci api-gateway deployment update --deployment-id $deployment_ocid --specification file://./"deployment_$timestamp.json"
+rm -f "_deployment_$timestamp.json"
 rm -f "deployment_$timestamp.json"
