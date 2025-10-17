f5925ac66bf5e6cbb0b6b91873b0d72a42a659d0
Mika Rinne <mika.rinne@oracle.com>
2025-10-17 18:58:21 +0300

    updates

--- cars-api-100/build.sh
+++ cars-api-100/build.sh
@@ -1,1 +1,1 @@
-docker build . --build-arg APIGW_PATH=/api -t fra.ocir.io/frsxwtjslf35/cars-api:1.0.0-free
+docker build . --build-arg APIGW_PATH=/api-100 -t fra.ocir.io/frsxwtjslf35/cars-api:1.0.0-free
--- cars-api-200/build.sh
+++ cars-api-200/build.sh
@@ -1,1 +1,1 @@
-docker build . --build-arg APIGW_PATH=/api -t fra.ocir.io/frsxwtjslf35/cars-api:2.0.0
+docker build . --build-arg APIGW_PATH=/api-200 -t fra.ocir.io/frsxwtjslf35/cars-api:2.0.0
