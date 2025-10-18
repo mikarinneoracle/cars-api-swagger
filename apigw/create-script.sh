b04be8da12944fff112af70ad2f648d4d120d076
Mika Rinne <mika.rinne@oracle.com>
2025-10-18 10:26:15 +0300

    www-data updated

--- apigw/create-script.sh
+++ apigw/create-script.sh
@@ -1,9 +1,8 @@
 source settings.env
 read -p "Enter Container Instances private IP:" ci_private_ip
 timestamp=$(date +%s)
-sed "s|{CI_PRIVATE_IP}|$ci_private_ip|g" deployment.json > "_deployment_$timestamp.json"
-sed "s|{AUTH_FUNC_OCID}|$auth_function_ocid|g" "_deployment_$timestamp.json" > "deployment_$timestamp.json"
+sed "s|{CI_PRIVATE_IP}|$ci_private_ip|g" deployment.json > "deployment_$timestamp.json"
+sed -i "s|{AUTH_FUNC_OCID}|$auth_function_ocid|g" "deployment_$timestamp.json"
 cat "deployment_$timestamp.json"
 oci api-gateway deployment create --compartment-id $compartment_ocid --gateway-id $gateway_ocid --path-prefix "/" --specification file://./"deployment_$timestamp.json"
-rm -f "_deployment_$timestamp.json"
 rm -f "deployment_$timestamp.json"
