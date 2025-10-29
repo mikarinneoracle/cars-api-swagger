ba773edc727588f29a5d67a44c2112f5d74f9b58
Mika Rinne <mika.rinne@oracle.com>
2025-10-29 21:14:30 +0200

    Logging fix

--- ci-sidecar/index.js
+++ ci-sidecar/index.js
@@ -1,4 +1,4 @@
-const TailFile = require('@logdna/tail-file')
+const tailfile = require('@logdna/tail-file')
 const identity = require("oci-identity");
 const common = require("oci-common");
 const loggingingestion = require("oci-loggingingestion");
@@ -11,7 +11,7 @@ const log_ocid = process.env.log_ocid;
 console.log("OCI LOG:" + log_ocid);
 
 const log_file = process.env.log_file;
-console.log("ACCESS LOG:" + log_file);
+console.log("LOG FILE:" + log_file);
 
 const www_path = process.env.www_path;
 console.log("WWW DATA:" + www_path);
@@ -25,8 +25,8 @@ console.log("OS BUCKET RELOAD (ms):" + reloadDelay);
 const logHeader = process.env.log_header;
 console.log("LOG HEADER:" + logHeader);
 
-//const provider = new common.ConfigFileAuthenticationDetailsProvider("config");
-const provider = common.ResourcePrincipalAuthenticationDetailsProvider.builder();
+const provider = new common.ConfigFileAuthenticationDetailsProvider("~/.oci/config");
+//const provider = common.ResourcePrincipalAuthenticationDetailsProvider.builder();
 
 const logClient = new loggingingestion.LoggingClient({ authenticationDetailsProvider: provider });
 
@@ -37,11 +37,15 @@ const nsResponse = await osClient.getNamespace(nsRequest);
 const namespace = nsResponse.value;
 
 mount(osClient, namespace, bucket, www_path, reloadDelay);
+    
+startTail(log_ocid, log_file, logHeader);
 
-const tail = new TailFile(log_file, {encoding: 'utf8'})
+async function startTail(log_ocid, log_file, logHeader)
+{
+  const tail = new tailfile(log_file, {encoding: 'utf8'})
   .on('data', (chunk) => {
-    console.log(`${chunk}`)
-    writeLog(logClient, log_ocid, logHeader, logHeader, `${chunk}`)
+    //console.log(`${chunk}`)
+    writeLog(logClient, logHeader, logHeader, log_file, `${chunk}`)
   })
   .on('tail_error', (err) => {
     console.error('TailFile had an error!', err)
@@ -51,8 +55,13 @@ const tail = new TailFile(log_file, {encoding: 'utf8'})
   })
   .start()
   .catch((err) => {
-    console.error('Cannot start.  Does " + log_file + "  exist?', err)
-  })
+    console.log("Cannot start.  Does " + log_file + "  exist?")
+    setTimeout(function() {
+        console.log("Trying again to open " + log_file + " ..");
+        startTail(log_ocid, log_file, logHeader);
+    }, 5000);
+  });
+ }
 }
 
 async function mount(osClient, namespace, bucket, www_path, reloadDelay)
