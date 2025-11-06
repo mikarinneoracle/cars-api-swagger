5b68d047318488ccb9e18fe21ff045f8c459a125
Mika Rinne <mika.rinne@oracle.com>
2025-11-06 12:11:03 +0200

    Dockerfile JDK updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -251,7 +251,7 @@ resource "oci_container_instances_container_instance" "container_instance" {
   }
 
   container_restart_policy = "ON_FAILURE"
-  display_name             = "Nginx with OCI SDK sidecar, Redis signup and PostgreSQL for API 3.0.0"
+  display_name             = "Nginx with OCI SDK sidecar, Redis signup and PostgreSQL for API 3.0.0 with Metrics"
 
   graceful_shutdown_timeout_in_seconds = "10"
 
